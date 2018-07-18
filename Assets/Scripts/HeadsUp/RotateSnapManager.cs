a8c03018fea2f17186d706b1b2db60fbff1406a0
Daniel Walsh <daniel_walsh@gensler.com>
2018-07-18 17:24:15 -0400

    removed mock object from scene

--- Assets/Scripts/HeadsUp/RotateSnapManager.cs
+++ Assets/Scripts/HeadsUp/RotateSnapManager.cs
@@ -1,12 +1,13 @@
-﻿namespace HeadsUp {
+﻿namespace HeadsUp
+{
 
     using System.Collections;
     using UnityEngine;
 
-    public class RotateSnapManager : SnapManager {
-
-        protected override void SetSnapVector() {
-
+    public class RotateSnapManager : SnapManager
+    {
+        protected override void SetSnapVector()
+        {
             startVector = menusManager.ThisModel.transform.rotation.eulerAngles;
             float snapped = Mathf.Round(startVector.y / snapValue) * snapValue;
             endVector = new Vector3(0, snapped, 0);
@@ -14,12 +15,13 @@
             handController.TransformValue = snapped;
         }
 
-        protected override IEnumerator LerpToSnap(Transform thisTransform, float duration) {
-
+        protected override IEnumerator LerpToSnap(Transform thisTransform, float duration)
+        {
             float t = 0;
-            while (t < 1) {
-                handController.OnClick();      
-                thisTransform.eulerAngles = Vector3.Lerp(startVector, endVector, t);   
+            while (t < 1)
+            {
+                handController.OnClick();
+                thisTransform.eulerAngles = Vector3.Lerp(startVector, endVector, t);
                 t += Time.deltaTime / duration;
                 handController.OnClick();
                 yield return null;
