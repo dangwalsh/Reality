0d814063bbb573e2aff24599032c7cc4737b2d92
Daniel Walsh <daniel_walsh@gensler.com>
2018-08-14 10:19:58 -0400

    deprecated unused classes

---
+++ Assets/Scripts/HeadsUp/Obsolete/LoadingCircle.cs
@@ -0,0 +1,22 @@
+﻿namespace HeadsUp
+{
+    using System;
+    using UnityEngine;
+
+    [Obsolete("Not in use", true)]
+    public class LoadingCircle : MonoBehaviour
+    {
+        private RectTransform rectComponent;
+        public float rotateSpeed = 200f;
+
+        private void Start()
+        {
+            rectComponent = GetComponent<RectTransform>();
+        }
+
+        private void Update()
+        {
+            rectComponent.Rotate(0f, 0f, -rotateSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/HeadsUp/Obsolete/SourceManager.cs
@@ -0,0 +1,20 @@
+﻿namespace HeadsUp
+{
+    using UnityEngine;
+    using HoloToolkit.Unity.InputModule;
+    using System;
+
+    [Obsolete("Not in use", true)]
+    public class SourceManager : MonoBehaviour, ISourceStateHandler
+    {
+        public void OnSourceDetected(SourceStateEventData eventData)
+        {
+            Debug.Log("Source Detected");
+        }
+
+        public void OnSourceLost(SourceStateEventData eventData)
+        {
+            Debug.Log("Source Lost");
+        }
+    }
+}
