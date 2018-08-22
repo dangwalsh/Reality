69a82631de11529a4473f38104328eb8b5fd5373
Daniel Walsh <daniel_walsh@gensler.com>
2018-08-22 11:20:16 -0400

    new splash screen

---
+++ Assets/Scripts/SplashAnimation.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SplashAnimation : MonoBehaviour
+{
+    public string loadLevel;
+    public Image splash;
+    public float delayTime = 1.0f;
+    public float fadeInTime = 2.5f;
+    public float fadOutTime = 2.5f;
+
+    void Awake()
+    {
+        splash.canvasRenderer.SetAlpha(0);
+    }
+
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(delayTime);
+        FadeIn();
+        yield return new WaitForSeconds(fadeInTime);
+        FadeOut();
+        yield return new WaitForSeconds(fadOutTime);
+        yield return new WaitForSeconds(delayTime);
+
+        SceneManager.LoadScene(loadLevel);
+    }
+
+    private void FadeIn()
+    {
+        splash.CrossFadeAlpha(1.0f, fadeInTime, false);
+    }
+
+    private void FadeOut()
+    {
+        splash.CrossFadeAlpha(0.0f, fadOutTime, false);
+    }
+}
---
+++ Assets/Scripts/SpriteFade.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SpriteFade : MonoBehaviour {
+
+    public string loadLevel;
+    SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.color = new Color(1, 1, 1, 0);
+    }
+
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(1.0f);
+        StartCoroutine(Fade(0, 1, 4));
+        yield return new WaitForSeconds(2.5f);
+        StartCoroutine(Fade(1, 0, 4));
+        yield return new WaitForSeconds(2.5f);
+
+        SceneManager.LoadScene(loadLevel);
+    }
+
+    IEnumerator Fade(float start, float end, float rate)
+    {
+        float t = 0;
+        while (t < 1)
+        {
+            t += Time.deltaTime * rate;
+            spriteRenderer.color = new Color(1, 1, 1, Mathf.Lerp(start, end, t));
+            yield return null;
+        }
+    }
+}
+
